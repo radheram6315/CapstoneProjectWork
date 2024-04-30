e92b6a01a692019bc4a773b98e128cd3e136b893
Radha Gunturu <radheram_a@yahoo.com>
2024-04-30 12:31:01 -0400

    starting point

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import { NewsReader } from './NewsReader';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <h1>News Reader App</h1>
+      </header>
+      <NewsReader />
+    </div>
+  );
+}
+
+export default App;
